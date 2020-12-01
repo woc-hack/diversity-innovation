71e3f0515484a9df7369354864732f8bb3acfc33
Sam Yong <myong@andrew.cmu.edu>
2020-12-01 00:23:37 -0500

    put aggregate entries into projects dir

--- aggregate-project-entries.sh
+++ aggregate-project-entries.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 while read p; do
-  zcat /da0_data/play/JSthruMaps/PtaPkgRJS.*.s | grep "$p" | sort > data/entries."$p".log
+  zcat /da0_data/play/JSthruMaps/PtaPkgRJS.*.s | grep "$p" | sort > data/projects/entries."$p".log
 done < data/project-finder.log
 
