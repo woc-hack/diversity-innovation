2f65c40d917566e16634ad8dc80d71268e2e276a
Sam Yong <myong@andrew.cmu.edu>
2020-12-02 11:33:18 -0500

    aggregate from sorted merged table

--- aggregate-project-entries.sh
+++ aggregate-project-entries.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 while read p; do
-  zcat /da0_data/play/JSthruMaps/PtaPkgRJS.*.s | grep "$p" | sort > data/projects/entries."$p".log
+  zcat /da0_data/play/JSthruMaps/tPaPkgRJS.s | grep "$p" > data/projects/entries."$p".log
 done <&0
 
