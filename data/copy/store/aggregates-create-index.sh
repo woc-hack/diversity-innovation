388a614cc74634ffebe61cb6941ef3b571bb4222
Sam Yong <myong@andrew.cmu.edu>
2021-05-16 15:54:09 -0400

    use temp store directory for innovation table index

--- data/copy/store/aggregates-create-index.sh
+++ data/copy/store/aggregates-create-index.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env sh
 
-sqlite3 innos.db "CREATE INDEX idx_author ON innovations(author);"
+sqlite3 innos.db << EOF
+  PRAGMA temp_store = 1;
+  PRAGMA temp_store_directory = '/home/SAMFYB/repos/diversity-innovation/data/copy/store/tmp';
 
-sqlite3 innos.db "CREATE INDEX idx_project ON innovations(project);"
+  PRAGMA temp_store;
+  PRAGMA temp_store_directory;
+
+  CREATE INDEX idx_author ON innovations(author);
+EOF
+
+sqlite3 innos.db << EOF
+  PRAGMA temp_store = 1;
+  PRAGMA temp_store_directory = '/home/SAMFYB/repos/diversity-innovation/data/copy/store/tmp';
+
+  PRAGMA temp_store;
+  PRAGMA temp_store_directory;
+
+  CREATE INDEX idx_project ON innovations(project);
+EOF
 
