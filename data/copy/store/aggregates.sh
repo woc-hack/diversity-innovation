50489a83f45621dd764a7ad4c4708f7226477fb6
Sam Yong <myong@andrew.cmu.edu>
2021-05-23 02:06:26 -0400

    sort aggregate counts by author and by project

--- data/copy/store/aggregates.sh
+++ data/copy/store/aggregates.sh
@@ -20,10 +20,13 @@
 #   CREATE INDEX idx_project ON innovations(project);
 # EOF
 
-# sqlite3 innos.db "SELECT MIN(timestamp), MAX(timestamp) FROM innovations;" > aggregates-timestamp-range.log
-# sqlite3 innos.db "SELECT COUNT(DISTINCT project) FROM innovations;" > aggregates-count-project.log
-# sqlite3 innos.db "SELECT COUNT(DISTINCT author) FROM innovations;" > aggregates-count-author.log
+# sqlite3 innos.db "SELECT MIN(timestamp), MAX(timestamp) FROM innovations;" > aggregates-timestamp-range.data
+# sqlite3 innos.db "SELECT COUNT(DISTINCT project) FROM innovations;" > aggregates-count-project.data
+# sqlite3 innos.db "SELECT COUNT(DISTINCT author) FROM innovations;" > aggregates-count-author.data
 
-# sqlite3 innos.db "SELECT COUNT(*), project FROM innovations WHERE impact >= 2 GROUP BY project;" > aggregates-count-by-project.log
-sqlite3 innos.db "SELECT COUNT(*), author FROM innovations WHERE impact >= 2 GROUP BY author;" > aggregates-count-by-author.log
+# sqlite3 innos.db "SELECT COUNT(*), project FROM innovations WHERE impact >= 2 GROUP BY project;" > aggregates-count-by-project.data
+# sqlite3 innos.db "SELECT COUNT(*), author FROM innovations WHERE impact >= 2 GROUP BY author;" > aggregates-count-by-author.data
+
+cat aggregates-count-by-project.data | sort -nr > aggregates-count-by-project.sort.data
+cat aggregates-count-by-author.data | sort -nr > aggregates-count-by-author.sort.data
 
